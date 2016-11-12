3a2b805ae142c917a50f5092d586fe3bc40252e2
Stringer86 <stringer.stephen@gmail.com>
2016-11-12 15:10:25 -0800

    html views set up

--- app/app.js
+++ app/app.js
@@ -4,4 +4,26 @@ import uiRouter from 'angular-ui-router';
 
 
 
-angular.module('myAngularApp', [angularMaterialize, uiRouter]);
+angular.module('my-app', [angularMaterialize, uiRouter])
+.config(['$stateProvider', ($stateProvider) => {
+  $stateProvider
+    .state('home', {
+      url: '',
+      templateUrl: 'views/home.html',
+    })
+    .state('main', {
+      url: '/',
+      templateUrl: 'views/home.html',
+    })
+    .state('city', {
+      url: '/city',
+      templateUrl: 'views/city.html',
+    })
+    .state('country', {
+      url: '/country',
+      templateUrl: 'views/country.html',
+    }).state('profile', {
+      url: '/profile',
+      templateUrl: 'views/profile.html'
+    });
+}]);
